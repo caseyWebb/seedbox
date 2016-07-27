b3d7777947f5ab2d49c2c817eaaa3f5556fee710
caseyWebb <notcaseywebb@gmail.com>
2016-07-27 17:23:26 -0400

    WIP

--- nginx/conf.d.template/make_configs.sh
+++ nginx/conf.d.template/make_configs.sh
@@ -1,7 +1,7 @@
-envsubst < /etc/nginx/conf.d/www.conf.template > /etc/nginx/conf.d/www.conf
-envsubst < /etc/nginx/conf.d/deluge.conf.template > /etc/nginx/conf.d/deluge.conf
-envsubst < /etc/nginx/conf.d/sonarr.conf.template > /etc/nginx/conf.d/sonarr.conf
-envsubst < /etc/nginx/conf.d/couchpotato.conf.template > /etc/nginx/conf.d/couchpotato.conf
-envsubst < /etc/nginx/conf.d/headphones.conf.template > /etc/nginx/conf.d/headphones.conf
-envsubst < /etc/nginx/conf.d/nzbget.conf.template > /etc/nginx/conf.d/nzbget.conf
-envsubst < /etc/nginx/conf.d/plex.conf.template > /etc/nginx/conf.d/plex.conf
+envsubst < /etc/nginx/conf.d.template/www.conf > /etc/nginx/conf.d/www.conf
+envsubst < /etc/nginx/conf.d.template/deluge.conf > /etc/nginx/conf.d/deluge.conf
+envsubst < /etc/nginx/conf.d.template/sonarr.conf > /etc/nginx/conf.d/sonarr.conf
+envsubst < /etc/nginx/conf.d.template/couchpotato.conf > /etc/nginx/conf.d/couchpotato.conf
+envsubst < /etc/nginx/conf.d.template/headphones.conf > /etc/nginx/conf.d/headphones.conf
+envsubst < /etc/nginx/conf.d.template/nzbget.conf > /etc/nginx/conf.d/nzbget.conf
+envsubst < /etc/nginx/conf.d.template/plex.conf > /etc/nginx/conf.d/plex.conf
